5f4bd53ca1b40a5f96ca41a2285da779f4c4f4bf
Matt Rieger <matthew.rieger@gmail.com>
2013-05-19 20:32:38 -0500

    made all purdy

--- header.php
+++ header.php
@@ -2,6 +2,8 @@
 <head>
   <title>My Web Languages</title>
   <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="css/bootstrap-responsive.css" rel="stylesheet">
   <link rel="stylesheet" type="text/css" href="index.css">
 </head>
 <body>
--- index.php
+++ index.php
@@ -1,48 +1,58 @@
 <?php require 'header.php';?>
-  <h1>My Programming Languages</h1>
-  <div class="lang_table float">
-    <table>
-      <tr>
-        <th>Languages I Know</th>
-        <th>Languages I Want to Know</th>
-      </tr>
-        <?php
-          @ $db = new mysqli('localhost', 'lang', 'lang123', 'languages');
+  <div class="lang_table container-fluid">
+    <div class="row-fluid">
+      <div class="span6 offset3">
+        <div class="hero-unit">
+          <h1>My Programming Languages</h1>
+        </div>
+      </div>
+    </div>
+    <div class="row-fluid">
+      <div class="span4 offset2">
+        <table class="table table-bordered">
+          <tr>
+            <th>Languages I Know</th>
+            <th>Languages I Want to Know</th>
+          </tr>
+          <?php
+            @ $db = new mysqli('localhost', 'lang', 'lang123', 'languages');
 
-          if(mysqli_connect_errno()) {
-            echo "<p><strong>Error</strong>: Could not connect to database. Please try again later.</p>";
-            exit;
-          }
-
-          $query_known = "select * from languages where known = 1";
-          $query_unknown = "select * from languages where known = 0";
-
-          $result_known = $db->query($query_known);
-          $result_unknown = $db->query($query_unknown);
-
-          $num_rows = max($result_known->num_rows, $result_unknown->num_rows);
-
-          for ($i=0; $i < $num_rows; $i++) { 
-            $row_known = $result_known->fetch_assoc();
-            $row_unknown = $result_unknown->fetch_assoc();
-            echo "<tr><td>".$row_known['name'];
-            if ($row_known) {
-              echo " <a href=\"switch_know.php?id=".$row_known['id']."\">>></a>";
+            if(mysqli_connect_errno()) {
+              echo "<p><strong>Error</strong>: Could not connect to database. Please try again later.</p>";
+              exit;
             }
 
-            echo "</td><td>".$row_unknown['name'];
-            if ($row_unknown) {
-              echo " <a href=\"switch_know.php?id=".$row_unknown['id']."\"><<</a>";
+            $query_known = "select * from languages where known = 1";
+            $query_unknown = "select * from languages where known = 0";
+
+            $result_known = $db->query($query_known);
+            $result_unknown = $db->query($query_unknown);
+
+            $num_rows = max($result_known->num_rows, $result_unknown->num_rows);
+
+            for ($i=0; $i < $num_rows; $i++) { 
+              $row_known = $result_known->fetch_assoc();
+              $row_unknown = $result_unknown->fetch_assoc();
+              echo "<tr><td>".$row_known['name'];
+              if ($row_known) {
+                echo " <a href=\"switch_know.php?id=".$row_known['id']."\"><i class=\"icon-chevron-right\"></i></a>";
+              }
+
+              echo "</td><td>".$row_unknown['name'];
+              if ($row_unknown) {
+                echo " <a href=\"switch_know.php?id=".$row_unknown['id']."\"><i class=\"icon-chevron-left\"></i></a>";
+              }
+              echo "</td></tr>";
             }
-            echo "</td></tr>";
-          }
-        ?>
-    </table>
-  </div>
-  <div class="lang_form float">
-    <form name="new_lang" method="post" action="new_lang.php">
-      <input type="text" name="name" value=""><br>
-      <input type="submit" value="Submit New Language">
-    </form>
+          ?>
+        </table>
+      </div>
+      <div class="lang_form span4 offset1">
+        <form name="new_lang" method="post" action="new_lang.php">
+          <input type="text" name="name" value=""><br>
+          <input type="submit" value="Submit New Language">
+        </form>
+      </div>
+    </div>
   </div>
 <?php require 'footer.php';?>
